5104308a3ac9d584e9232ac4fc375a8fef3f210f
r1oga <r1oga@tuta.io>
2020-06-26 14:41:40 +0200

    feat: Add type guard on email prop

--- src/routes.ts
+++ src/routes.ts
@@ -20,7 +20,12 @@ router.get('/login', (req: Request, res: Response) => {
 
 router.post('/login', (req: Request, res: Response) => {
   const { email, password } = req.body
-  res.send(email + password)
+
+  if (email) {
+    res.send(email.toUpperCase())
+  } else {
+    res.send('You must provide an email')
+  }
 })
 
 export { router }
