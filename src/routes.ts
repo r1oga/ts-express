3ac4f55400d5e579f1b04104ee7755d4d0b7874a
r1oga <r1oga@tuta.io>
2020-06-26 14:45:19 +0200

    refactor: Extend Request interface

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,8 @@
 import { Response, Request, Router } from 'express'
 
+interface ReqWithBody extends Request {
+  body: { [key: string]: string | undefined }
+}
 const router = Router()
 
 router.get('/login', (req: Request, res: Response) => {
@@ -18,7 +21,7 @@ router.get('/login', (req: Request, res: Response) => {
   `)
 })
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: ReqWithBody, res: Response) => {
   const { email, password } = req.body
 
   if (email) {
