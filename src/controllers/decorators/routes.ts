73e891586cffb00e6cfefbca64ebdda32c552d5c
r1oga <r1oga@tuta.io>
2020-06-27 15:08:16 +0200

    refactor: Use enum to represent metadata keys

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
 import { Methods } from './Methods'
+import { MetadataKeys } from './MetadataKeys'
 
 // returns a decorator factory
 const routeBinder = (method: string) => (path: string) => (
@@ -7,8 +8,8 @@ const routeBinder = (method: string) => (path: string) => (
   key: string,
   desc: PropertyDescriptor
 ) => {
-  Reflect.defineMetadata('path', path, target, key)
-  Reflect.defineMetadata('method', method, target, key)
+  Reflect.defineMetadata(MetadataKeys.path, path, target, key)
+  Reflect.defineMetadata(MetadataKeys.method, method, target, key)
 }
 
 export const get = routeBinder(Methods.get)
